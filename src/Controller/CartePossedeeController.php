4d92bcba70bbe3695fb07af5b1c3039a7d96a7d5
mouhamed lamine kebe <mouhamed-lamine.kebe@etu.univ-lehavre.fr>
2024-03-21 15:34:01 +0100

    implementation de l'entity cartePossedee

--- src/Controller/CartePossedeeController.php
+++ src/Controller/CartePossedeeController.php
@@ -6,14 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class CollectionController extends AbstractController
+class CartePossedeeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('collection/index.html.twig', [
-            'controller_name' => 'CollectionController',
+            'controller_name' => 'CartePossedeeController',
         ]);
     }
+    // //ajouter une collection
+    // #[Route('/collection/insert', name: 'insert_collection')]
 
 }
